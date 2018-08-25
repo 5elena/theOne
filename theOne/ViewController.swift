cc77a1a337e570644bde42099acd860834f0ee7d
Selena <selena@2001-b011-400a-1062-0c17-78c5-037f-cf9c.dynamic-ip6.hinet.net>
2018-08-26 01:13:18 +0800

    Initial Commit

---
+++ theOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  theOne
+//
+//  Created by Selena on 2018/8/26.
+//  Copyright © 2018年 Selena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
